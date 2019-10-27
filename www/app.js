a112de668097ddfa6a0521c48cf2233838edacae
Nasa Nguyen <nasa8x@gmail.com>
2019-10-27 08:32:49 +0700

    update demo

--- www/app.js
+++ www/app.js
@@ -5,9 +5,9 @@ import '../dist/v-markdown-editor.css';
 
 import Vue from 'vue';
 import App from './app.vue';
-// import Editor from '../dist';
+import Editor from '../dist';
 
-// Vue.use(Editor);
+Vue.use(Editor);
 
 new Vue({
   el: '#app', 
